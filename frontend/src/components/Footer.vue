3739f738d73c19306dfc13fea7051f7422fdac9a
Jangchaeyun <62280276+Jangchaeyun@users.noreply.github.com>
2022-05-11 00:52:47 +0900

    master

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+    <footer>&copy;2022 My Memo App. All rights reserved.</footer>
+</template>
+
+<style scoped>
+footer {
+    background: #C8C8FF;
+    border-top: 1px solid #eee;
+    padding: 25px;
+}
+</style>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+    <header>My Memo App</header>
+</template>
+
+<style scoped>
+header {
+    text-align: center;
+    padding: 25px;
+    border-bottom: 1px solid #eee;
+    background: #C8C8FF;
+}
+</style>
